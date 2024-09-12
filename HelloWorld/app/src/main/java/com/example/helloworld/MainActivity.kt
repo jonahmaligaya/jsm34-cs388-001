776a94c36eec0a8f31f9029174da77e2faffbbf1
joeconlan <150975713+jonahmaligaya@users.noreply.github.com>
2024-09-11 23:29:18 -0400

    Added HelloWorld.

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.say_hello_button)
+        button.setOnClickListener {
+            Log.v("Hello world","Button clicked!")
+            Toast.makeText(this, "Hello to you too!", Toast.LENGTH_SHORT).show()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
